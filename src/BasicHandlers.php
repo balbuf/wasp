67d6a9a887ac35a5add76681c5354a2aee648104
Stephen Beemsterboer <balbuf@gmail.com>
2017-07-23 14:06:22 -0400

    Add autoloader handler

--- src/BasicHandlers.php
+++ src/BasicHandlers.php
@@ -4,6 +4,7 @@ namespace OomphInc\FAST_WP;
 use OomphInc\FAST_WP\Compilable\FunctionExpression;
 use OomphInc\FAST_WP\Compilable\ArrayExpression;
 use OomphInc\FAST_WP\Compilable\TranslatableTextExpression;
+use OomphInc\FAST_WP\Compilable\RawExpression;
 
 class BasicHandlers {
 
@@ -52,7 +53,48 @@ class BasicHandlers {
 			return new TranslatableTextExpression($label);
 		}, $data);
 
-		$transformer->setup_file->add_expression(new FunctionExpression('register_nav_menus', [new ArrayExpression($data)]));
+		$transformer->setup_file->add_expression(new FunctionExpression('register_nav_menus', [new ArrayExpression($data)]), 'after_setup_theme');
+	}
+
+	public static function autoloader($transformer, $data) {
+		if (!isset($data['namespace'])) {
+			echo "Error: no namespace set for autoloader property\n";
+			return;
+		}
+
+		$data += ['dir' => 'src'];
+		$autoloader = <<<'PHP'
+/**
+ * PSR 4 Autoloader for class includes.
+ * @source  http://www.php-fig.org/psr/psr-4/examples/
+ */
+spl_autoload_register( function ( $class ) {
+	// project-specific namespace prefix
+	$prefix = %PREFIX%;
+	// base directory for the namespace prefix
+	$base_dir = __DIR__ . %DIR%;
+	// does the class use the namespace prefix?
+	$len = strlen( $prefix );
+	if ( strncmp( $prefix, $class, $len ) !== 0 ) {
+		// no, move to the next registered autoloader
+		return;
+	}
+	// get the relative class name
+	$relative_class = substr( $class, $len );
+	// replace the namespace prefix with the base directory, replace namespace
+	// separators with directory separators in the relative class name, append
+	// with .php
+	$file = $base_dir . str_replace( '\\', '/', $relative_class ) . '.php';
+	// if the file exists, require it
+	if ( file_exists( $file ) ) {
+		require $file;
+	}
+} );
+PHP;
+		$autoloader = str_replace('%PREFIX%', var_export((string) $data['namespace'], true), $autoloader);
+		$autoloader = str_replace('%DIR%', var_export('/' . trim((string) $data['dir'], '/') . '/', true), $autoloader);
+
+		$transformer->setup_file->add_expression(new RawExpression($autoloader));
 	}
 
 }
