a2e49bc53d447aef15870320d0a257a59f8b5a33
Stephen Beemsterboer <balbuf@gmail.com>
2018-03-04 16:08:12 -0500

    Fix text_domain prop reference

--- src/Compilable/TranslatableTextExpression.php
+++ src/Compilable/TranslatableTextExpression.php
@@ -8,7 +8,7 @@ class TranslatableTextExpression extends BaseCompilable {
 
 	public function compile() {
 		$compiled = $this->transformer->compile((string) $this->text);
-		if ($domain = $this->transformer->getProperty('text_domain')) {
+		if ($domain = $this->transformer->getProperty('localization', 'text_domain')) {
 			$compiled = '__( ' . $compiled . ', ' . $this->transformer->compile($domain) . ' )';
 		}
 		return $compiled;
