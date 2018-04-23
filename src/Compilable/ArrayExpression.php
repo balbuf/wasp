b2dcebfb2f0320aa54b6fd85ba4685c75105da6a
Stephen Beemsterboer <balbuf@gmail.com>
2018-04-23 15:55:51 -0400

    Fix array compilation for empty arrays

--- src/Compilable/ArrayExpression.php
+++ src/Compilable/ArrayExpression.php
@@ -8,6 +8,10 @@ class ArrayExpression extends BaseCompilable {
 	public $inline = false;
 
 	public function compile() {
+		if (empty($this->array)) {
+			return '[]';
+		}
+
 		// check if this is an associative array or not
 		$isAssoc = array_keys($this->array) !== range(0, count($this->array) - 1);
 		return ($this->inline ? '[' : "[\n") . implode($this->inline ? ', ' : ",\n", array_map(function($key, $value) use ($isAssoc) {
